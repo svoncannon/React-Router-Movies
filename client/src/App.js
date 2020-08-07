2e03e5eaf8129282c7b669e8cde4a56d2fbb7701
Samantha <toxicwow21@gmail.com>
2020-08-07 12:06:01 -0400

    finished mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -26,10 +30,20 @@ const App = () => {
   };
 
   return (
+    <Router>
     <div>
       <SavedList list={[ /* This is stretch */]} />
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+      <Route path ='/movies/:id'>
+        <Movie />
+      </Route>
+
+      <Route path = '/'>
+        <MovieList movies = {movieList}/>
+      </Route>
+      </Switch>
     </div>
+    </Router>
   );
 };
 
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
 
 const SavedList = props => (
   <div className="saved-list">
@@ -6,7 +8,11 @@ const SavedList = props => (
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
+    <Router>
+    <Link to= '/'>
     <div className="home-button">Home</div>
+    </Link>
+    </Router>
   </div>
 );
 
